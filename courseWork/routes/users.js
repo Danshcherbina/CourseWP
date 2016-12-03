79f31055004d286d9f088d83e17b26ba8628cc27
Danshcherbina <danshcherbina@gmail.com>
2016-12-03 10:33:38 +0200

    final k2

--- courseWork/routes/users.js
+++ courseWork/routes/users.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
 
-var UserTemplate = require('../user_template');
-/*var mongo_db = 'mongodb://localhost/kursach';
-mongoose.connect(mongo_db);*/
+var UserTemplate = require('../user_template').UserTemplate;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -18,67 +15,18 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/:id', function(req, res, next) {
-  UserTemplate.findById(req.params.id, (err, user) => {
-    if(!user){
-      res.statusCode = 404;
-      res.send('Not Found');
-    }
-    if(!err){
-      res.send(user);
-    } else {
-      res.statusCode = 500;
-      res.send('Server error');
-    }
-  });
-});
-
-
-router.put('/:id', (req,res) => {
-  UserTemplate.findById(req.params.id, (err, user) => {
-    if(!user) {
-      res.statusCode = 404;
-      return res.send('Not Found');
-    }
-    if(req.body.username !== null)
-      user.username = req.body.username;
-    if(req.body.password !== null)
-      user.password = req.body.password;
-    if(req.body.email !== null)
-      user.email = req.body.email;
-    if(req.body.image !== null)
-      user.image = req.body.image;
-    user.save((err) => {
-      if(!err){
-        res.send({Status : 'ok\n', user : user});
-      } else {
-        console.log(err);
-        if(err.name == 'ValidationError') {
-                 res.statusCode = 400;
-                 res.send({ error: 'Validation error' });
-             } else {
-                 res.statusCode = 500;
-                 res.send({ error: 'Server error' });
-             }
-      }
-    });
-  })
-})
-
 router.post('/', function(req, res) {
   var user = new UserTemplate({
     username : req.body.username,
     email : req.body.email,
     password : req.body.password,
     image : req.body.image,
+    admin : false,
   });
 
   user.save((err) => {
-
     if(!err){
-
       res.send({Status : 'ok\n', user : user});
-
     } else {
       console.log(err);
       if(err.name == 'ValidationError') {
@@ -91,6 +39,8 @@ router.post('/', function(req, res) {
     }
   });
 });
+
+
 
 router.delete('/:id', (req, res) => {
     UserTemplate.findById(req.params.id, (err, user) => {
@@ -108,4 +58,5 @@ router.delete('/:id', (req, res) => {
       });
     });
 });
+
 module.exports = router;
